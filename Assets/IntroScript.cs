6e7d75fd046c4306cba7897e002d85652fdd2b02
pascal <pascal.greim@rub.de>
2020-03-30 03:15:37 +0200

    Fixed saving bug

--- Assets/IntroScript.cs
+++ Assets/IntroScript.cs
@@ -10,6 +10,10 @@ public class IntroScript : MonoBehaviour
     [SerializeField] TMP_Text DialogText=default;
     [SerializeField] GameObject Canvas=default;
     [SerializeField] Animator animatorBG = default;
+
+
+    [SerializeField] PlayerStats playerStats = default;
+    [SerializeField] ShopStatistics shopStatistics = default;
     bool interactable = false;
     int currentText = 0;
     string[] texts = { "Brave soul", "A blood moon has risen as the abyss summons its hordes",
@@ -23,6 +27,20 @@ public class IntroScript : MonoBehaviour
     {
         DialogText.text = texts[0];
         Canvas.SetActive(false);
+
+
+        playerStats.maxHealth = 5;
+        playerStats.maxMunition = 3;
+        playerStats.meleeDamage = 1;
+        playerStats.money = 0;
+        playerStats.munition = 0;
+        playerStats.rangedDamage = 2;
+
+        shopStatistics.maxHealthUpgradeCount = 0;
+        shopStatistics.maxMunitionUpgradeCount = 0;
+        shopStatistics.meleeDamageUpgradeCount = 0;
+        shopStatistics.munitionUpgradeCount = 0;
+        shopStatistics.rangedDamageUpgradeCount = 0;
     }
 
     // Update is called once per frame
--- Assets/UI Elements/MainMenuScript.cs
+++ Assets/UI Elements/MainMenuScript.cs
@@ -30,19 +30,6 @@ public class MainMenuScript : MonoBehaviour {
         PlayerPrefs.DeleteAll();
         PlayerPrefs.SetInt("GameStarted", 1);
 
-        playerStats.maxHealth = 5;
-        playerStats.maxMunition = 3;
-        playerStats.meleeDamage = 1;
-        playerStats.money = 0;
-        playerStats.munition = 0;
-        playerStats.rangedDamage = 2;
-
-        shopStatistics.maxHealthUpgradeCount = 0;
-        shopStatistics.maxMunitionUpgradeCount = 0;
-        shopStatistics.meleeDamageUpgradeCount = 0;
-        shopStatistics.munitionUpgradeCount = 0;
-        shopStatistics.rangedDamageUpgradeCount = 0;
-
         SceneManager.LoadScene(4);
 
     }
